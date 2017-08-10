00f780e834169f84be85e5b2377e65c422051060
6apc1k <livee.in@gmail.com>
2017-08-10 23:44:58 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,532 @@
+
+var studentMock = [{
+        name: 'Liudmyla',
+        lastName: 'Bashta',
+        img: 'http://68.media.tumblr.com/f29f8c1bcd90c046d3b2f740d3e8fa43/tumblr_inline_o10mvwbyEt1rxrd1y_500.jpg',
+        coverImg: 'http://i2.kym-cdn.com/photos/images/facebook/000/243/865/8f3.jpg',
+        email: 'liudmyla.bashta@gmail.com',
+        skills: ['Javascript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Roman',
+        lastName: 'Chapkailo',
+        img: 'https://s-media-cache-ak0.pinimg.com/736x/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5--pikachu-halloween-costume-diy-halloween-costumes.jpg',
+        coverImg: 'http://fbcovershub.com/media/cover-250-beautiful-seaside-fb-cover-1388015476.jpg',
+        email: 'romafromukraine@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Khrystyna',
+        lastName: 'Dalivska',
+        img: 'https://ichef-1.bbci.co.uk/news/660/cpsprodpb/169F6/production/_91026629_gettyimages-519508400.jpg',
+        coverImg: 'https://sky.easypano.com/EPSUpload2/Pano/2017/04-12/12/636275969355928205/560_315.jpg',
+        email: 'khrystynadalivska@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Ivan',
+        lastName: 'Gnatyshyn',
+        img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+        coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+        email: 'gnatyshyn.ivan@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Andrii',
+        lastName: "Hun'ka",
+        img: 'https://cdn.pixabay.com/photo/2017/01/06/19/15/soap-bubble-1958650_960_720.jpg',
+        coverImg: 'http://i.dailymail.co.uk/i/pix/2017/01/16/20/332EE38400000578-4125738-image-a-132_1484600112489.jpg',
+        email: 'andriyggg@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Maksym',
+        lastName: 'Izmailov',
+        img: 'https://cdn.pixabay.com/photo/2016/04/17/10/38/doberman-1334497_960_720.jpg',
+        coverImg: 'https://cdn.pixabay.com/photo/2016/03/06/05/03/sunrise-1239728_960_720.jpg',
+        email: 'maksym.izmailov.lv@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Taras',
+        lastName: 'Kharkhalis',
+        img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+        coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+        email: 'taraskharkhalis97@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Khrystia',
+        lastName: 'Kondratovych',
+        img: 'https://www.webdesign.org/img_articles/21726/17.jpg',
+        coverImg: 'https://cdn.pixabay.com/photo/2016/10/13/10/37/dandelion-1737324_960_720.jpg',
+        email: 'khrustyk@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Iuliia',
+        lastName: 'Kurylo',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSljkhtuNjmEz2YeriPLPdntnTKNAwXFOAQSx1u6yAkPhYYw8-Pnw',
+        coverImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT-qepvLICH8tsGuZqbZwpO7rk5afp274Lu4bgjai8Uo30gDKifA',
+        email: 'iulia.kurylo@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Roman',
+        lastName: 'Mandziuk',
+        img: 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg',
+        coverImg: 'http://html.com/wp-content/uploads/very-large-flamingo.jpg',
+        email: 'rmandzyuk94@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Oleh',
+        lastName: 'Marko',
+        img: 'https://www.petdrugsonline.co.uk/images/page-headers/cats-master-header',
+        coverImg: 'http://wiki-carpathians.com/wp-content/uploads/2015/02/climate-carpathians.jpg',
+        email: 'olehmarko@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Anatoliy',
+        lastName: 'Mazur',
+        img: 'https://static.pexels.com/photos/20787/pexels-photo.jpg',
+        coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+        email: 'mail4tolik@gmail.com',
+        skills: ['JavaScript', 'CSS', 'HTML']
+    },
+    {
+        name: 'Vitaliy',
+        lastName: 'Palyushok',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwJ09w5uUaTy-_Z30WFuwflgr_jrtaana_tNkRV_RXA5JMt2lu',
+        coverImg: 'http://facebookcovers.piz18.com/wp-content/uploads/2012/04/geek-fb-cover.jpg',
+        email: 'xskeletons@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Stepan',
+        lastName: 'Prokopiak',
+        img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+        coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+        email: 'sprokopyak96@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Stepan',
+        lastName: 'Sendun',
+        img: 'http://i.piccy.info/i9/a25aa836358fb23a05d6e9207c976dd9/1500482900/30480/1163444/537377255ws00241_57th_annua.jpg',
+        coverImg: 'http://i.piccy.info/i9/b311de1aaff52532b361a178e8e35de4/1500482959/135850/1163444/0008540461_10.jpg',
+        email: 'steve.neeson21@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Andrii',
+        lastName: 'Soroka',
+        img: 'https://static.pexels.com/photos/33537/cat-animal-cat-portrait-mackerel.jpg',
+        coverImg: '',
+        email: 'andrii.soroka@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Illya',
+        lastName: 'Syniuk',
+        img: 'https://i.ytimg.com/vi/YgmIibSnZs0/maxresdefault.jpg',
+        coverImg: 'https://www.facebook.com/photo.php?fbid=578802345636154&set=a.326403767542681.1073741828.100005191805447&type=3&theater',
+        email: 'illyasynuik@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Andrew',
+        lastName: 'Tantsiura',
+        img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+        coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+        email: 'andrii.tans@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Liliya',
+        lastName: 'Tserkovna',
+        img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/c0.17.160.160/p160x160/14725559_311214412585028_1352062715786494390_n.jpg?oh=b2cbcb3de774187b75d5253a276bc2f7&oe=59F5D47B',
+        coverImg: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/14368772_295189700854166_8626244722206545788_n.jpg?oh=02cf7516f9337bc439a42595ff893821&oe=5A051EC4',
+        email: 'lilichkaTserkovna@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    },
+    {
+        name: 'Anton',
+        lastName: 'Zhygalov',
+        img: 'http://static.tvtropes.org/pmwiki/pub/images/Hello_Kitty_Pink_2981.jpg',
+        coverImg: 'https://thumb1.shutterstock.com/display_pic_with_logo/156640/208511908/stock-photo-arad-romania-september-hello-kitty-pattern-printed-on-cardboard-box-studio-shot-208511908.jpg',
+        email: 'antonzhygalov@gmail.com',
+        skills: ['JavaScript', 'HTML', 'CSS']
+    }
+];
+
+var index = null;
+
+createTable();
+createForm();
+
+/////////////////////////////
+////////// UTILS
+////////////////////////////
+
+function createEl(tagName) {
+    return document.createElement(tagName);
+}
+
+function sortByKey(array, key, flag) {
+    if (!key) {
+        return;
+    }
+
+    return array.sort(function(a, b) {
+        var x, y;
+        x = flag ? a[key] : b[key];
+        y = flag ? b[key]: a[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateSymbols(input) {
+    var re = /^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$/; 
+    return re.test(input);
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+//////////////////////////////
+//////////////TABLE
+//////////////////////////////
+
+function getTable() {
+    var table = document.querySelector('#studentTable');
+
+    if (!table) {
+        table = createEl('table');
+        table.setAttribute('id', 'studentTable');
+        table.appendChild(createHeader());
+        table.appendChild(createEl('tbody'));
+
+        table.classList.add('table');
+        table.classList.add('table-hover');
+
+        container.appendChild(table);
+    }
+
+    return table.querySelector('tbody');
+}
+
+function createTable() {
+    var table = getTable();
+
+    studentMock.map(function(elem) {
+        return {
+            name: elem.name + " " + elem.lastName,
+            email: elem.email,
+            'profile picture': elem.img,
+            skills: elem.skills,
+            controls: ''
+        };
+    }).forEach(function(item) {
+        table.appendChild(createRow(item));
+    });
+}
+
+function refreshTable() {
+    document.querySelectorAll('#studentTable tr').forEach(function(el) {
+        el.remove();
+    })
+    createTable();
+}
+
+function createHeader() {
+    var dataKeys = ['Student', 'email', 'Profile picture', 'Skills'];
+    var tHeader = createEl('thead');
+    var headerLabels = JSON.parse(JSON.stringify(dataKeys));
+    headerLabels.push('controls'); // placeholder for extra buttons
+
+    for (var i = 0; i < headerLabels.length; i++) {
+        var th = createEl('th');
+        th.innerHTML = headerLabels[i];
+        th.style.padding = '10px';
+        if (i === 0 || i === 1 || i === 3) {
+            var glyph = createEl('span');
+            glyph.classList.add('glyphicon', 'glyphicon-sort');
+            glyph.style.transform = 'translateX(10px)';
+            sortListener(th);
+            th.appendChild(glyph);
+        }
+        tHeader.appendChild(th);
+    }
+
+    return tHeader;
+}
+
+function createRow(item) {
+    var tr = createEl('tr');
+
+    for (var key in item) {
+        var td = createEl('td');
+        td.onclick = function() {
+            var target = event.target || event.srcElement;
+            if (target.nodeName === 'TD') {
+                alert(target.parentElement.firstChild.innerHTML);
+            } else {
+                alert(target.parentElement.parentElement.firstChild.innerHTML);
+            }
+        }
+
+        if (key === 'controls') {
+            var edit = createEl('div');
+            edit.classList.add('glyphicon', 'glyphicon-edit');
+            edit.setAttribute('title', 'edit user');
+
+            //////////////
+            //EDIT
+            //////////////
+
+            edit.onclick = function(e) {
+
+                var target = e.target || e.srcElement;
+                e.preventDefault();
+                e.stopPropagation();
+                var check = target.parentElement.parentElement.children[1].innerHTML; // selecting e-mail of specified element
+               
+                for (var i = 0; i < studentMock.length; i++) {
+                    if (studentMock[i].email === check) {
+                        setFormData(studentMock[i]);
+                        index = i;
+                    }
+                }
+            }
+            var division = document.createElement('div');
+
+            var remove = document.createElement('div');
+            remove.classList.add('glyphicon', 'glyphicon-trash');
+            remove.setAttribute('title', 'delete user');
+
+            ///////////////
+            //REMOVE
+            //////////////
+
+            remove.onclick = function(e) {
+
+                var target = e.target || e.srcElement;
+                e.preventDefault();
+                e.stopPropagation();
+                var check = target.parentElement.parentElement.children[1].innerHTML; // selecting e-mail of specified element
+
+                for (var i = 0; i < studentMock.length; i++) {
+                    if (studentMock[i].email === check) {
+                        studentMock.splice(i, 1);
+                    }
+                }
+                refreshTable();
+            }
+
+            td.appendChild(edit);
+            td.appendChild(division);
+            td.appendChild(remove);
+        } else if (key === 'profile picture') {
+            var myImg = createEl('img');
+            myImg.setAttribute('src', item[key]);
+            myImg.setAttribute('width', '250px');
+            td.appendChild(myImg);
+        } else {
+            td.innerHTML = item[key];
+        }
+        tr.appendChild(td);
+    }
+    return tr;
+}
+
+////////////////////////////
+/////// FORM 
+///////////////////////////
+
+function createForm() {
+    var formLabels = ['Name', 'Last name', 'Email', 'Profile picture', 'Skills'];
+    var form = createEl('form');
+    form.setAttribute('id', 'studentForm');
+    form.style.margin = '20px 0 20px 0px';
+
+    formLabels.forEach(function(name) {
+        // label tag
+        var label = createEl('label');
+        
+        label.setAttribute('for', name);
+        label.innerHTML = name;
+
+        // input tag
+        var input = createEl('input');
+        input.setAttribute('name', name);
+        input.style.display = 'block';
+        input.style.border = '1px solid #000';
+
+        form.appendChild(label);
+        form.appendChild(input);
+    });
+
+    form.appendChild(createButton('Save', saveForm));
+    form.appendChild(createButton('Cancel', clearForm));
+
+    var table = document.querySelector('#studentTable');
+    container.insertBefore(form, table);
+}
+
+function createButton(name, fn) {
+    var btn = createEl('button');
+    btn.classList.add('btn', 'btn-success');
+    btn.setAttribute('name', name);
+    btn.setAttribute('type', 'Button');
+    btn.innerHTML = name;
+    btn.style.margin = '10px 10px 20px 0';
+    btn.onclick = function() {
+        fn();
+    };
+
+    return btn;
+}
+
+function getForm() {
+    return document.querySelector('#studentForm');
+}
+
+function getFormData() {
+    var form = getForm().querySelectorAll('input');
+    var obj = {};
+
+    for (var i in form) {
+        if (typeof form[i] === 'object') {
+            obj[form[i].name] = form[i].value;
+        }
+    }
+
+    return obj;
+}
+
+function setFormData(obj) {
+    var formPushKeys = ['name', 'lastName', 'email', 'img', 'skills'];
+    var form = getForm().querySelectorAll('input');
+    for (var i = 0; i < form.length; i++) {
+        form[i].value = obj[formPushKeys[i]];
+    }
+}
+
+function checkForm() {
+    var form = getForm().querySelectorAll('input');
+    for (var i = 0; i < form.length; i++) {
+        if (!form[i].value) {
+            alert('Cells should not be empty');
+            throw new Error('Cells should not be empty');
+        }
+        if (i === 0 || i === 1 || i === 4) {
+            if (!validateSymbols(form[i].value)) {
+                alert(`Please, correct: ${form[i].value}. It should not contain special symbols or numbers`);
+                throw new Error('Enter correct email'); 
+            }
+        }
+    }
+    if (!validateEmail(form[2].value)) {
+        alert('Enter correct email');
+        throw new Error('Enter correct email');
+    }
+
+}
+
+function saveForm() {
+    var formData = getFormData();
+    var item = {
+        name: formData.Name,
+        lastName: formData['Last name'],
+        img: formData['Profile picture'],
+        email: formData.Email,
+        skills: formData.Skills.split(' ,')
+    };
+    
+    checkForm();
+
+    getTable().appendChild(createRow(item));
+
+    if (index !== null) {
+        studentMock.splice(index, 1, item);
+        index = null;
+    } else {
+        studentMock.push(item);
+    }
+
+    clearForm();
+    refreshTable();
+
+    alert('Table has been updated!');
+}
+
+function clearForm() {
+    getForm().reset();
+}
+
+////////////////////////////////////////////////
+// EVENT LISTENERS
+///////////////////////////////////////////////
+
+function sortListener(el) {
+    el.addEventListener('click', function() {
+        var icon = this.children[0];
+
+        for (var i = 0; i < studentMock.length; i++) {
+           if (Array.isArray(studentMock[i]['skills'])) {
+               studentMock[i]['skills'] = studentMock[i]['skills'].join(',');
+           }
+       }
+    
+        var sort = {
+            cls: 'glyphicon-sort-by-alphabet',
+            altCls: 'glyphicon-sort-by-alphabet-alt',
+            flag: true,
+            rules: {
+                Student: 'name',
+                email: 'email',
+                Skills: 'skills',
+                controls: false
+            }
+        };  
+
+        if (hasClass(icon, sort.altCls) || (!hasClass(icon, sort.cls) && !hasClass(icon, sort.altCls))) {
+            setDefaultClasses();
+            icon.classList.add(sort.cls);
+        } else {
+            setDefaultClasses();
+            icon.classList.add(sort.altCls);
+            sort.flag = false;
+        }
+       
+        sortByKey(studentMock, sort.rules[this.textContent], sort.flag);
+            
+        refreshTable();
+
+        function setDefaultClasses() {
+            document.querySelectorAll('th span').forEach(function(elem) {
+                elem.classList = '';
+                elem.classList.add('glyphicon', 'glyphicon-sort');
+            });
+            icon.className = 'glyphicon';
+        }
+
+    for (var i = 0; i < studentMock.length; i++) {
+           if (!Array.isArray(studentMock[i]['skills'])) {
+               studentMock[i]['skills'] = studentMock[i]['skills'].split(',');
+           }
+       }
+          
+    }, false);
+}
+
+
+
